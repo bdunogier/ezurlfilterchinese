42240113fe7f656b69c2b32e28d3d78eab17332e
EAB Development Team <xian@eab.co.uk>
2013-04-18 16:29:43 +0800

    Updated to reflect changes in eZ Publish and add the option to capitalize each pinyin word
    1. The eZURLFilter was replaced by eZURLAliasFilter.
    2. Added a switch in site.ini to control whether each Chinese character
    in the pinyin URL is capitalized or not

--- urlfilters/ezurlfilterchinesetransliteration.php
+++ urlfilters/ezurlfilterchinesetransliteration.php
@@ -1,16 +1,42 @@
 <?php
-class eZURLFilterChineseTransliteration
+
+class eZURLFilterChineseTransliteration extends eZURLAliasFilter
 {
-    function process( $text, $languageObject, $caller )
-    {
-        if ( $languageObject->Locale == 'chi-CN' )
-        {
-            return transliterate( $text, array( 'han_transliterate', 'diacritical_remove' ), 'utf-8', 'utf-8' );
-        }
-        else
-        {
-            return $text;
-        }
-    }
+	/**
+	* Empty constructor
+	*/
+	public function __construct() {}
+
+	/**
+	* Transliterate the Chinese name of the object being published using pinyin
+	*
+	* @param string The text of the URL alias
+	* @param object The eZContentObject object being published
+	* @params object The eZContentObjectTreeNode in which the eZContentObject is published
+	* @return string The transformed URL alias with the nodeID
+	*/
+	public function process( $text, &$languageObject, &$caller )
+	{
+		if( !$caller instanceof eZContentObjectTreeNode )
+		{		
+			eZDebug::writeError( 'The caller variable was not an eZContentObjectTreeNode', __METHOD__ );
+			return $text;
+		}
+
+		if ( $languageObject->Locale == 'chi-CN' )
+		{
+			$transliterated = transliterate( $text, array( 'han_transliterate', 'diacritical_remove' ), 'utf-8', 'utf-8' );
+				
+			$upword = eZINI::instance('site.ini');
+			$changewords = $upword->variable( 'ChineseURLAlias', 'Capitalize' );
+			if( $changewords )
+				return(str_replace(' ', '', ucwords($transliterated)));
+			else
+				return(str_replace(' ', '', $transliterated )); 
+		}
+		else
+			return $text;
+	}
 }
+
 ?>
